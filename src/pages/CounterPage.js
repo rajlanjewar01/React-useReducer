acdd6a3517a1d7bd8c30f76a7ae471c3b4d0bd36
rajlanjewar01 <rajl.perficient@gmail.com>
2026-01-02 18:24:33 +0530

    Adding new State Update

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,8 +1,11 @@
 import { useReducer } from 'react';
 import Button from '../components/Button';
 
+// Constant action types
 const INCREMENT_COUNT = 'increment';
 const SET_VALUE_TO_ADD = 'change_value_to_add';
+const DECREMENT_COUNT = 'decrement';
+const ADD_VALUE_TO_COUNT = 'add_value_to_count';
 
 const reducer = (state, action) => {
 	switch(action.type) {
@@ -10,6 +13,19 @@ const reducer = (state, action) => {
 			return {
 				...state,
 				count: state.count + 1
+			};
+
+		case DECREMENT_COUNT:
+			return {
+				...state,
+				count: state.count - 1
+			};
+		
+		case ADD_VALUE_TO_COUNT:
+			return {
+				...state,
+				count: state.count + state.valueToAdd,
+				valueToAdd: 0
 			};
 
 		case SET_VALUE_TO_ADD:
@@ -43,7 +59,9 @@ function CounterPage({ initialCount }) {
 	}
 
 	const decrement = () => {
-		setCount(count - 1);
+		dispatch({
+			type: DECREMENT_COUNT
+		});
 	}
 
 	const handleChange = (event) => {
@@ -51,14 +69,15 @@ function CounterPage({ initialCount }) {
 		dispatch({
 			type: SET_VALUE_TO_ADD,
 			payload: value
-		})
+		});
 	}
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
 
-		setCount(count + valueToAdd);
-		setValueToAdd(0);
+		dispatch({
+			type: ADD_VALUE_TO_COUNT
+		});
 	}
 
 	return (
